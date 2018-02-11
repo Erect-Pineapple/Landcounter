c40f5eb06078ee2584913a3026a26a1bbac3c9df
Erect-Pineapple <36353397+Erect-Pineapple@users.noreply.github.com>
2018-02-11 00:10:05 -0800

    Add files via upload
    Added code that displays a message if the suggested number of lands does not match the desired amount due to rounding.  Also added some descriptions in the code.

--- Simple MTG land base program.py
+++ Simple MTG land base program.py
@@ -1,6 +1,7 @@
-print("This is my first custom program.")
-
-print("I will tell you how many basics to run of each type.")
+# First ask the player questions to determine deck parameters.
+print("How many basic lands do you want to run?")
+basics = int(input())
+print("This program will tell you how many basic lands to run of each type.")
 print("How many White symbols are in your deck?")
 whites = int(input())
 print("How many Blue symbols are in your deck?")
@@ -11,21 +12,31 @@ print("How many Black symbols are in your deck?")
 blacks = int(input())
 print("How many Green symbols are in your deck?")
 greens = int(input())
-print("How many basic lands are you running?")
-basics = int(input())
 total = whites + blues + reds + blacks + greens
 
+# Determine correct number of basic lands for each mana type and define them.
 plainsnum = round((whites / total) * basics)
 islandsnum = round((blues / total) * basics)
 swampsnum = round((blacks / total) * basics)
 mountainsnum = round((reds / total) * basics)
 forestsnum = round((greens / total) * basics)
 
-print(f"Since you are running {basics} basic lands, you will need:")
+# Displays the optimal number of each basic land based on the math above.
+print(f"With {basics} basic lands you should use the following pile.")
 print(f"Plains: {plainsnum}")
 print(f"Islands: {islandsnum}")
 print(f"Swamps: {swampsnum}")
 print(f"Mountains: {mountainsnum}")
 print(f"Forests: {forestsnum}")
 
+# Displays action to take if rounding error happens.
+landtotal = plainsnum + islandsnum + swampsnum + mountainsnum + forestsnum
+if basics - landtotal != 0: # only shows if the number of lands suggested does not match amount desired.
+    print("There is a rounding error,")
+if basics - landtotal > 0:
+    print(f"add {basics - landtotal} land to a pile.")
+if basics - landtotal < 0:
+    print(f"remove {landtotal - basics} land from a pile.")
+print()
+print("Program made by Erect Pineapple.")
 input()
